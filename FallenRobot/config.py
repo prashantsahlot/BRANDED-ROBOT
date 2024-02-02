d384668915002dd15b16e1a71db1e09fa264df0a
prashantsahlot <156554454+prashantsahlot@users.noreply.github.com>
2024-02-02 16:03:20 +0530

    config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,13 +16,13 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/82f1053c956dd6256ff1e.jpg"
 
-    SUPPORT_CHAT = "BRANDED_WORLD"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "AKATSUKI_OFFICIAL_0"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6258877205  # User id of your telegram account (Must be integer)
+    OWNER_ID = 1668494031  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
